76cdabbf514f2ccc177a70357dec12aeabef79b9
Jennifer Ried <jried@rheinahrcampus.de>
2021-11-04 16:03:03 +0100

    Icon Anpassung

--- MP3-Player/player.cpp
+++ MP3-Player/player.cpp
@@ -17,21 +17,40 @@ Player::Player(QWidget *parent)
     //https://www.flaticon.com/search/2?word=next&order_by=4&type=icon
     QIcon* zurueck = new QIcon(":/Bilder/last.png");
     QIcon* weiter = new QIcon(":/Bilder/next-button.png");
-    //https://www.flaticon.com/free-icon/stop_727260?term=stop&page=1&position=27&page=1&position=27&related_id=727260&origin=search
+    //https://www.flaticon.com/search?word=stop&type=icon
     QIcon* stopp = new QIcon(":/Bilder/stop.png");
     //https://www.flaticon.com/search?word=silent&order_by=4&type=icon
     QIcon* stumm = new QIcon(":/Bilder/silent.png");
     //https://www.flaticon.com/search?word=pause&order_by=4&type=icon
     QIcon* pause = new QIcon(":/Bilder/pause.png");
 
+    QSize iconSize_zurueckspulen_vorspulen;
+    iconSize_zurueckspulen_vorspulen.setWidth(60);
+    iconSize_zurueckspulen_vorspulen.setHeight(60);
+
+    QSize iconSize;
+    iconSize.setWidth(52);
+    iconSize.setHeight(52);
+
     ui->wiedergabe_pause_button->setIcon(*wiedergabe);
+    ui->wiedergabe_pause_button->setIconSize(iconSize);
     ui->zurueckspulen_button->setIcon(*zurueckspulen);
+    ui->zurueckspulen_button->setIconSize(iconSize_zurueckspulen_vorspulen);
     ui->vorspulen_button->setIcon(*vorspulen);
+    ui->vorspulen_button->setIconSize(iconSize_zurueckspulen_vorspulen);
     ui->zufall_button->setIcon(*zufall);
+    ui->zufall_button->setIconSize(iconSize);
     ui->zurueckspringen_button->setIcon(*zurueck);
+    ui->zurueckspringen_button->setIconSize(iconSize);
     ui->weiter_button->setIcon(*weiter);
+    ui->weiter_button->setIconSize(iconSize);
     ui->stopp_button->setIcon(*stopp);
+    ui->stopp_button->setIconSize(iconSize);
     ui->stumm_button->setIcon(*stumm);
+    ui->stumm_button->setIconSize(iconSize);
+
+
+    ui->widget->setVisible(false);
 
 }
 
